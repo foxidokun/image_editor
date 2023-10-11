d351a010e8565073160b5a4d7650dbe0530786b7
foxidokun <fox@forestfox.tk>
2023-10-11 04:16:18 +0300

    fix incorrect moving

--- src/canvas.cpp
+++ src/canvas.cpp
@@ -22,14 +22,20 @@ EVENT_RES Canvas::on_mouse_release(const mouse_event_t& key) {
 }
 
 EVENT_RES Canvas::on_mouse_move(const mouse_event_t& key) {
-    if (is_drawing) {
-        Point target = Point(key.x - _pos.x, key.y - _pos.y);
-        click_info_t click_info = {target, last_pos, start_pos};
+    bool hit_y = key.y >= _pos.y && key.y <= _pos.y + _size.y;
+    bool hit_x = key.x >= _pos.x && key.x <= _pos.x + _size.x;
+    if (hit_x && hit_y) {
+        if (is_drawing) {
+            Point target = Point(key.x - _pos.x, key.y - _pos.y);
+            click_info_t click_info = {target, last_pos, start_pos};
 
-        tool_manager->paint(_data, click_info);
-        
-        last_pos = target;
-        _data.display();
+            tool_manager->paint(_data, click_info);
+            
+            last_pos = target;
+            _data.display();
+        }
+        return EVENT_RES::STOP;
+    } else {
+        return EVENT_RES::CONT;
     }
-    return EVENT_RES::STOP;
 }
